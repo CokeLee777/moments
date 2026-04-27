51cd1bbb1282000bae364b8498b70c1dbba7cab4
CokeLee777 <akak640@kyonggi.ac.kr>
2026-04-27 19:52:36 +0900

    fix(functions): clean up jest.config.js - remove redundant preset, narrow transform pattern

--- apps/functions/jest.config.js
+++ apps/functions/jest.config.js
@@ -1,13 +1,12 @@
 // apps/functions/jest.config.js
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@moments/shared$': '<rootDir>/../../packages/shared/src/index.ts',
   },
   testMatch: ['**/__tests__/**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
   },
 };
